8038301a97036b03c665d7ff5a0204566f3f9c43
AbuTalhaRaj <128001740+AbuTalhaRaj@users.noreply.github.com>
2024-03-30 23:21:33 +0600

    Update task_manager.py

--- colab_leecher/utility/task_manager.py
+++ colab_leecher/utility/task_manager.py
@@ -69,7 +69,7 @@ async def taskScheduler():
                 ida = "♻️"
             elif "magnet" in link or "torrent" in link:
                 ida = "🧲"
-                Messages.caution_msg = "\n\n⚠️<i><b> Torrents Are Strictly Prohibited in Google Colab</b>, Try to avoid Magnets !</i>"
+                Messages.caution_msg = "\n\n⚠️<i><b> Torrents Are Strictly Prohibited!</b>, Try to avoid Torrent/Magnets !</i>"
             elif "youtube.com" in link or "youtu.be" in link:
                 ida = "🏮"
             else:
